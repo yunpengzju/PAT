56a139d9739bc790ca9f766dcf83417fb691895c
turkey <mathcyp2008@hotmail.com>
2013-09-28 23:12:15 +0800

    1,开始编写乙级pat考试练习题

---
+++ basic1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	int count = 0;
+	scanf("%d",&n);
+	while(1 != n)
+	{
+		if(n%2 == 0)  n/=2;
+		else n = (3*n+1)/2;
+		count++;
+	}
+	printf("%d",count);
+	return 0;
+}
